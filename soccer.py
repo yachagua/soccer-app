6834b0ab7206928cbf948caaac1e254594660829
Yair Chaverra <yairchaverra@gmail.com>
2025-09-19 13:29:54 -0500

    Actualizar soccer.py con sustitucion y otras cositas

--- soccer.py
+++ soccer.py
@@ -63,9 +63,9 @@ if st.session_state.active_tab == "⚽ Eventos":
         "Evento",
         [
             "Comienza el encuentro","Asistencia","Balón Perdido","Duelo Ganado","Duelo Perdido","Falta cometida",
-            "Falta recibida","Lesionado","Gol","Pase clave","Pase completado","Pase perdido","Recuperaciones",
+            "Falta recibida","Fuera de lugar","Lesionado","Gol","Pase clave","Pase completado","Pase perdido","Recuperaciones",
             "Regate Exitoso","Regate Fallido","Tarjeta amarilla","Tarjeta roja","Tiro al arco",
-            "Tiro desviado","Tiro de esquina","Medio Tiempo","Finaliza el encuentro","Marcador Final"
+            "Tiro desviado","Tiro de esquina","Comienza la segunda parte","Medio Tiempo","Finaliza el encuentro","Marcador Final","Sustitución"
         ]
     )
 
